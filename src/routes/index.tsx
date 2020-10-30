63d660fe82d83d123381a4fee4b7c1eb9b072772
Jonatas Souza <jonatasfelipe2@hotmail.com>
2020-10-30 15:13:17 -0300

    setup env production

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
+
+    <Route path="/dashboard" isPrivate component={Dashboard} />
+    <Route path="/profile" isPrivate component={Profile} />
+  </Switch>
+);
+
+export default Routes;
